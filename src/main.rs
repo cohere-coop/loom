41febde197113c89db4db2ddcb521f5a3f8c3e33
Betsy Haibel <betsy.haibel@gmail.com>
2018-01-17 15:29:01 -0500

    Can kick off and listen to the results of processes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let child = Command::new("ls")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+
+    let output = child
+        .wait_with_output()
+        .expect("failed to wait on child");
+
+    match output.status.code() {
+        Some(code) => println!("process exited with code: {}", code),
+        None       => println!("process terminated by signal")
+    }
+
+    match String::from_utf8(output.stdout) {
+        Ok(output_string) => println!("process output: {}", output_string),
+        Err(e)            => println!("could not parse process output: {:?}", e)
+    }
 }
