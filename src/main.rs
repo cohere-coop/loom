6c1080a634bcd630be8985b6340389218094fb0b
Betsy Haibel <betsy.haibel@gmail.com>
2018-01-17 20:23:33 -0500

    Switch to using tokio_process for better async

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use std::process::{Command, Stdio};
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_process;
+
+use std::process::{Command, Output};
+use std::io::{self, Write};
+
+use tokio_core::reactor::Core;
+use tokio_process::CommandExt;
 
 fn main() {
-    let child = Command::new("ls")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to execute child");
+    ::std::process::exit(match run_core_loop() {
+       Ok(_) => 0,
+       Err(err) => {
+           writeln!(io::stderr(), "error: {:?}", err).unwrap();
+           1
+       }
+    });
+}
 
-    let output = child
-        .wait_with_output()
-        .expect("failed to wait on child");
+fn run_core_loop() -> Result<(), (std::io::Error)> {
+    match Core::new() {
+        Ok(mut unwrapped_core) => {
+            let output = run_command_on_core(&mut unwrapped_core, &mut Command::new("ls"));
+            print_output_details(output);
+            Ok(())
+        },
+        Err(e) => { Err(e) } 
+    }
+}
 
+fn run_command_on_core(core: &mut Core, command: &mut Command) -> Output {
+    let child = command.output_async(&core.handle());
+    core.run(child).expect("failed to capture child output")
+}
+
+fn print_output_details(output: Output) {
     match output.status.code() {
         Some(code) => println!("process exited with code: {}", code),
         None       => println!("process terminated by signal")
     }
-
     match String::from_utf8(output.stdout) {
-        Ok(output_string) => println!("process output: {}", output_string),
-        Err(e)            => println!("could not parse process output: {:?}", e)
+        Ok(output_string) => println!("process output:\n{}", output_string),
+        Err(e)            => println!("could not parse process output:\n {:?}", e)
     }
 }
