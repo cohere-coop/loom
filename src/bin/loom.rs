f38ca2e6a41065bc8aa79d7580d8287e83a0577b
Betsy Haibel <betsy.haibel@gmail.com>
2018-02-24 16:58:41 -0500

    ugly config file

--- src/bin/loom.rs
+++ src/bin/loom.rs
@@ -2,13 +2,27 @@ extern crate futures;
 extern crate tokio_core;
 extern crate tokio_process;
 extern crate tokio_io;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
 
 extern crate loom;
 
 use std::io::{self, Write};
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
 
 use loom::CommandParams;
 use loom::runner::run_commands;
+
+type Config<'a> = HashMap<String, CommandConfig<'a>>;
+
+#[derive(Deserialize)]
+struct CommandConfig<'a> {
+    name: &'a str,
+    arg: &'a str
+}
 
 fn main() {
     ::std::process::exit(match run_core_loop() {
@@ -21,5 +35,16 @@ fn main() {
 }
 
 fn run_core_loop() -> Result<(), (std::io::Error)> {
-    run_commands(vec![CommandParams::new("ping", "wecohere.com"), CommandParams::new("ping", "google.com")])
+
+    let mut config_toml = String::new();
+    File::open("./loomfile.toml").unwrap().read_to_string(&mut config_toml);
+
+    let config: Config = toml::from_str(&config_toml).unwrap();
+
+    let params_vec: Vec<CommandParams> = config
+        .into_iter()
+        .map(|(_, pair): (_, CommandConfig)| { CommandParams::new(pair.name, pair.arg) })
+        .collect();
+
+    run_commands(params_vec)
 }
